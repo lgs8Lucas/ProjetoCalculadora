273f276f8e11061c0d85ad8d3a2568548ee7755b
Lucas <lgs.08lucas@gmail.com>
2024-06-07 17:22:42 -0300

    At 2024-06-07 implementado função para limpar a memória da calculadora e para adicionar os dígitos

--- calculadora-react/src/main/Calculator.jsx
+++ calculadora-react/src/main/Calculator.jsx
@@ -3,7 +3,20 @@ import './Calculator.css'
 import Button from "../components/Button";
 import Display from "../components/Display";
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0,0],
+    current: 0
+}
+
+
+
 export default class Calculator extends Component{
+    
+    state = { ...initialState }
+
     constructor(props){
         super();
         this.clearMemory = this.clearMemory.bind(this)
@@ -12,20 +25,34 @@ export default class Calculator extends Component{
     }
 
     clearMemory(){
-        console.log('Limpar');
+        this.setState({ ...initialState })
     }
     setOperation(operation){
         console.log(operation);
     }
     addDigit(n){
-        console.log(n);
+        if(n === '.' && this.state.displayValue.includes('.'))
+            return;
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
+        const currentValue = clearDisplay? '' : this.state.displayValue;
+        const displayValue = currentValue + n;
+        this.setState({displayValue, clearDisplay:false});
+
+        if (n !== '.') {
+            const i = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const values = [ ...this.state.values];
+            values[i] = newValue;
+            this.setState({values});
+            console.log(values);
+        }
     }
 
     render(){
         return (
             <div className="calculator">
-                <Display value={100} />
-                <Button label="AC" click={this.clearMemory()} triple/>
+                <Display value={this.state.displayValue} />
+                <Button label="AC" click={this.clearMemory} triple/>
                 <Button label="/" click={this.setOperation} operation/>
                 <Button label="7" click={this.addDigit}/>
                 <Button label="8" click={this.addDigit}/>
